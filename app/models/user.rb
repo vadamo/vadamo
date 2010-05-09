c5e2bc1985d76fdf3a4459ffb99f4800e4e5e674
Kyle <Kyle.Randolph@gmail.com>
2010-05-08 19:34:18 -0700

    Done with user edit/update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20100507022008
+# Schema version: 20100509021438
 #
 # Table name: users
 #
@@ -11,6 +11,7 @@
 #  encrypted_password :string(255)
 #  salt               :string(255)
 #  remember_token     :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
