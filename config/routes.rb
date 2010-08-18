d3d669bd0cace60945f2cbfdafeb8b224740beba
Kyle <Kyle.Randolph@gmail.com>
2010-08-17 23:04:45 -0700

    new bundler 1.rc

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ ActionController::Routing::Routes.draw do |map|
   map.albums   '/albums',   :controller => 'albums', :action => 'show'
   map.privacy_policy   '/privacy_policy',   :controller => 'pages', :action => 'privacy_policy'
   map.terms_of_use   '/terms_of_use',   :controller => 'pages', :action => 'terms_of_use'
+  map.sitemap  '/sitemap',   :controller => 'pages', :action => 'sitemap'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
