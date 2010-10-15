f1da103cdd72d707e2c7164149cd8ecf405ab1d7
Kyle <Kyle.Randolph@gmail.com>
2010-10-14 23:11:13 -0700

    Inital new commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vadamo::Application.config.secret_token = '609936514f30f18ef5fd89f9def1e0a3fd491db6f2b160ccb483c45035d4f5c3dad9d31f4cbcbfd811f389b35ba53d96850af75cb1843158f4be612548de3c43'
