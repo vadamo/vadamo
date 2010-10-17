1a8d885806bf0c0b2b66709792e629ddfcbe22f1
Kyle <Kyle.Randolph@gmail.com>
2010-10-16 18:00:33 -0700

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,8 @@ module Vadamo
         ActiveSupport::Dependencies.mechanism = :load
       end
     end
-
+    
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
