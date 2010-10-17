ea8857e2db1d2bb9038247fe675f4377a03fccca
Kyle <Kyle.Randolph@gmail.com>
2010-10-16 20:40:57 -0700

    Added user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Vadamo::Application.routes.draw do
   get "sessions/new"
 
   resources :users
+  resources :sessions,   :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
 
-  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
